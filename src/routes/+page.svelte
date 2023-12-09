74d3835a485c91f2947641bd4697ae10d3893684
silvereengames <silvereennetwork@gmail.com>
2023-12-09 00:06:28 -0500

    undo the test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
   <h1 class="text-left text-4xl inline-block">What is Corrupted?</h1>
   <br />
   <p class="text-left w-1/2 text-xl inline-block">
-    Corrupted is a super amazing website where you can play many games from retro-style games to flash games. We have the ultamate game library for retro-style games and games you can not play anywhere else online. Lets get playing!
+    Corrupted is a website where you can play many games from retro-style games to flash games. We have the ultamate game library for retro-style games and games you can not play anywhere else online. Lets get playing!
   </p>
   <div class="w-1/3 h-10"></div>
   <img src="/games/slope.jpg" class="w-48 h-48 inline-block ml-10 rounded-md cursor-pointer hover:opacity-75" alt="slope" />
