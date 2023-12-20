52233551bea9ed0c42e75de22603efb576945e40
Silvereen <silvereennetwork@gmail.com>
2023-12-20 13:24:48 -0500

    Update layout.svelte

--- src/components/layout.svelte
+++ src/components/layout.svelte
@@ -9,23 +9,6 @@
 <meta content="#1f2937" data-react-helmet="true" name="theme-color" />
 <title>{pagetitle}</title>
 <slot />
-<!-- ADS - DO NOT REMOVE -->
-  <!--Ads-->
-  <script async src=
-  "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7001820938357122"
-  crossorigin="anonymous"></script><!--Ads-->
-  <script async src=
-  "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7001820938357122"
-  crossorigin="anonymous"></script><!-- Google tag (gtag.js) -->
-  <script async src=
-  "https://www.googletagmanager.com/gtag/js?id=G-9VYHCJ4PES"></script>
-  <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-9VYHCJ4PES');
-  </script>
 <div class="bg"></div>
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap");
