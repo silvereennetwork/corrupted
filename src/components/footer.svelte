6b9fd10756e5a5b4f619e247704ee09b074cf4de
3kh0 <echo-the-coder@tuta.io>
2024-01-18 10:17:42 -0500

    tos?

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,7 +1,7 @@
 <footer class="w-11/12 mx-auto h-18 bg-indigo-700 text-center nav flex flex-col justify-around items-center py-3 rounded-lg shadow-lg my-3">
   <p class="text-2xl">© 2023 Corrupted. Made with ❤️ by Silvereen Network</p>
   <div class="flex justify-center space-x-4 text-lg">
-    <a href="/terms-of-service" class="duration-150 hover:opacity-25">Terms of Service</a>
+    <a href="/tos" class="duration-150 hover:opacity-25">Terms of Service</a>
     <a href="https://discord.gg/discord" class="duration-150 hover:opacity-25" target="_blank" rel="noopener noreferrer">Discord</a>
     <a href="https://github.com/silvereennetwork/corrupted" class="duration-150 hover:opacity-25" target="_blank" rel="noopener noreferrer">GitHub</a>
   </div>
